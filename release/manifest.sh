07489fe1a656e6915d78a56178deb3505206fe25
Venil Noronha <veniln@vmware.com>
2020-06-17 22:34:11 -0700

    Build for Istio 1.5.3
    Signed-off-by: Venil Noronha <veniln@vmware.com>

---
+++ release/manifest.sh
@@ -0,0 +1,30 @@
+DEPENDENCIES=$(cat <<EOD
+  istio:
+    git: https://github.com/vmware-allspark/istio
+    branch: build-1.5.3
+  cni:
+    git: https://github.com/vmware-allspark/cni
+    auto: deps
+  api:
+    git: https://github.com/vmware-allspark/api
+    auto: modules
+  proxy:
+    git: https://github.com/vmware-allspark/proxy
+    auto: deps
+  pkg:
+    git: https://github.com/vmware-allspark/pkg
+    auto: modules
+  client-go:
+    git: https://github.com/vmware-allspark/client-go
+    branch: build-1.5.3
+  gogo-genproto:
+    git: https://github.com/vmware-allspark/gogo-genproto
+    branch: build-1.5.3
+  test-infra:
+    git: https://github.com/vmware-allspark/test-infra
+    branch: build-1.5.3
+  tools:
+    git: https://github.com/vmware-allspark/tools
+    branch: build-1.5.3
+EOD
+)
