0e54eb98057e133ce8819606437d717ef4d5ff6f
Venil Noronha <veniln@vmware.com>
2020-09-21 10:56:03 -0700

    Build for v1.6.9

---
+++ release/manifest.sh
@@ -0,0 +1,34 @@
+DEPENDENCIES=$(cat <<EOD
+  istio:
+    git: https://github.com/vmware-allspark/istio
+    branch: build-1.6.9
+  cni:
+    git: https://github.com/vmware-allspark/cni
+    auto: deps
+  api:
+    git: https://github.com/vmware-allspark/api
+    auto: modules
+  proxy:
+    git: https://github.com/vmware-allspark/proxy
+    auto: deps
+  pkg:
+    git: https://github.com/vmware-allspark/pkg
+    auto: modules
+  client-go:
+    git: https://github.com/vmware-allspark/client-go
+    branch: build-1.6.9
+    goversionenabled: true
+  gogo-genproto:
+    git: https://github.com/vmware-allspark/gogo-genproto
+    branch: build-1.6.9
+  test-infra:
+    git: https://github.com/vmware-allspark/test-infra
+    branch: build-1.6.9
+  tools:
+    git: https://github.com/vmware-allspark/tools
+    branch: build-1.6.9
+  envoy:
+    git: https://github.com/vmware-allspark/envoy
+    auto: proxy_workspace
+EOD
+)
