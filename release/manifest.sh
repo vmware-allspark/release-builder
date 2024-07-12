5a2708abd8be9046fb0fdafe088b407fa7ee6438
nsxsm-devops <nsxsm-devops@vmware.com>
2024-07-12 02:27:10 -0700

    Push from nsxm-devops allspark-istio-release-builder bd84dbd3

---
+++ release/manifest.sh
@@ -0,0 +1,28 @@
+DEPENDENCIES=$(cat <<EOD
+  istio:
+    git: https://github.com/vmware-allspark/istio
+    branch: build-1.21.4-release-v1
+  api:
+    git: https://github.com/vmware-allspark/api
+    auto: modules
+  proxy:
+    git: https://github.com/vmware-allspark/proxy
+    auto: deps
+  client-go:
+    git: https://github.com/vmware-allspark/client-go
+    branch: build-1.21.4-release-v1
+    goversionenabled: true
+  gogo-genproto:
+    git: https://github.com/vmware-allspark/gogo-genproto
+    branch: build-1.21.4-release-v1
+  test-infra:
+    git: https://github.com/vmware-allspark/test-infra
+    branch: build-1.21.4-release-v1
+  tools:
+    git: https://github.com/vmware-allspark/tools
+    branch: build-1.21.4-release-v1
+  envoy:
+    git: https://github.com/vmware-allspark/envoy
+    auto: proxy_workspace
+EOD
+)
